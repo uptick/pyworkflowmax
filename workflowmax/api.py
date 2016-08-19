a37288cb47ea7b5d547c7ed6b7b5aa28a6d9b583
Jarek Glowacki <jarekwg@gmail.com>
2016-08-19 10:54:56 +1000

    Check credentials; sort repr output

--- workflowmax/api.py
+++ workflowmax/api.py
@@ -1,3 +1,4 @@
+from .credentials import Credentials
 from .endpoints import ENDPOINTS
 from .managers import Manager
 
@@ -6,6 +7,12 @@ class WorkflowMax:
     """An ORM-like interface to the WorkflowMax API"""
 
     def __init__(self, credentials):
+        if not isinstance(credentials, Credentials):
+            raise TypeError(
+                'Expected a Credentials instance, got %s.' % (
+                    type(credentials).__name__,
+                )
+            )
         self.credentials = credentials
 
         for k, v in ENDPOINTS.items():
@@ -13,5 +20,5 @@ class WorkflowMax:
 
     def __repr__(self):
         return '%s:\n    %s' % (self.__class__.__name__, '\n    '.join(
-            v['plural'] for v in ENDPOINTS.values()
+            sorted(v['plural'] for v in ENDPOINTS.values())
         ))
