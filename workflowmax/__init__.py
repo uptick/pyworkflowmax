7d0b0ac2655d3a3ca3ff7c912f0cf8ce4577891d
Jarek Glowacki <jarekwg@gmail.com>
2016-08-19 11:39:24 +1000

    Bump version

--- workflowmax/__init__.py
+++ workflowmax/__init__.py
@@ -1,6 +1,6 @@
 from .api import WorkflowMax  # noqa
 from .credentials import Credentials  # noqa
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 
 __version__ = '.'.join(str(x) for x in VERSION[:(2 if VERSION[2] == 0 else 3)])  # noqa
